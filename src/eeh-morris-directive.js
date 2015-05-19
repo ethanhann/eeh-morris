df8d807158cfbef5eaa7c153c8cbbd0e74e25974
Ethan Hann <ethanhann@gmail.com>
2015-05-19 14:55:37 -0400

    Add shortcut directives

--- src/eeh-morris-directive.js
+++ src/eeh-morris-directive.js
@@ -1,21 +1,36 @@
 (function (angular) {
     'use strict';
 
-    angular.module('eehMorris').directive('eehMorris', function (eehMorris) {
+    function MorrisDirective (eehMorris) {
         return {
             restrict: 'AE',
+            template: '<div class="eeh-morris"></div>',
             scope: {
                 config: '=',
                 chartType: '='
             },
             link: function (scope, element) {
-                scope.config.element = element;
+                scope.config.element = element.find('.eeh-morris');
                 eehMorris[scope.chartType](scope.config);
             }
         };
-    });
+    }
 
-    angular.module('eehMorris').directive('eehMorrisBar', function () {
+    angular.module('eehMorris').directive('eehMorris', MorrisDirective);
+
+    function MorrisDonutDirective() {
+        return {
+            restrict: 'AE',
+            template: '<eeh-morris config="config" chart-type="\'Donut\'"></eeh-morris>',
+            scope: {
+                config: '='
+            }
+        };
+    }
+
+    angular.module('eehMorris').directive('eehMorrisDonut', MorrisDonutDirective);
+
+    function MorrisBarDirective () {
         return {
             restrict: 'AE',
             template: '<eeh-morris config="config" chart-type="\'Bar\'"></eeh-morris>',
@@ -23,5 +38,32 @@
                 config: '='
             }
         };
-    });
+    }
+
+    angular.module('eehMorris').directive('eehMorrisBar', MorrisBarDirective);
+
+    function MorrisLineDirective () {
+        return {
+            restrict: 'AE',
+            template: '<eeh-morris config="config" chart-type="\'Line\'"></eeh-morris>',
+            scope: {
+                config: '='
+            }
+        };
+    }
+
+    angular.module('eehMorris').directive('eehMorrisLine', MorrisLineDirective);
+
+    function MorrisAreaDirective () {
+        return {
+            restrict: 'AE',
+            template: '<eeh-morris config="config" chart-type="\'Area\'"></eeh-morris>',
+            scope: {
+                config: '='
+            }
+        };
+    }
+
+    angular.module('eehMorris').directive('eehMorrisArea', MorrisAreaDirective);
+
 })(angular);
